024e92eb100bce4d509af48f737c857071e5a4c7
alejandro <alejandrober96@gmail.com>
2023-05-15 15:03:17 -0500

    Add public and private methods for class person

--- person.rb
+++ person.rb
@@ -8,7 +8,22 @@ class Person
     @age = age
     @parent_permission = parent_permission
   end
+
+  def of_age?
+    return true unless @age < 18
+
+    false
+  end
+
+  def can_use_services?
+    return true if of_age? || @parent_permission
+
+    false
+  end
+
+  private :of_age?
 end
 
 person1 = Person.new(26, 'alejandro')
 puts(person1.name)
+puts person1.can_use_services?
