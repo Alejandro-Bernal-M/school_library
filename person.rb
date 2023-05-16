5e09469eece1819954f64a8121aebb0783701eb3
alejandro <alejandrober96@gmail.com>
2023-05-16 14:13:58 -0500

    Made person class inherent from the base component and add tests

--- person.rb
+++ person.rb
@@ -1,12 +1,20 @@
-class Person
+require './nameable'
+require './capitalize'
+require './trimmer'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
-
+  
   def initialize(age, name = 'unknow', parent_permission: true)
     @id = Random.rand(1..100)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+  
+  def correct_name
+    @name
   end
 
   def of_age?
@@ -24,6 +32,12 @@ class Person
   private :of_age?
 end
 
-person1 = Person.new(26, 'alejandro')
-puts(person1.name)
-puts person1.can_use_services?
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+puts capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+puts capitalizedTrimmedPerson.correct_name
+
+
+
