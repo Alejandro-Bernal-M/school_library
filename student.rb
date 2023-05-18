545570d46f7353c997ac48539cca85d45c6338b4
alejandro <alejandrober96@gmail.com>
2023-05-18 17:52:37 -0500

    Corrections

--- student.rb
+++ student.rb
@@ -4,8 +4,8 @@ class Student < Person
   attr_reader :classroom
   attr_accessor :parent_permission
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+  def initialize(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
   end
 
   def classroom=(classroom)
--- teacher.rb
+++ teacher.rb
@@ -3,8 +3,8 @@ require './person'
 class Teacher < Person
   attr_accessor :name
 
-  def initialize(age, specialization, name = 'unknow', parent_permission: true)
-    super(age, name, parent_permission)
+  def initialize(age, specialization, name, parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
