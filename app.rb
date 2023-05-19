d70caa1f6038561386e9089d344d7df7751697d6
alejandro <alejandrober96@gmail.com>
2023-05-19 10:31:20 -0500

    Change menu to main file

--- app.rb
+++ app.rb
@@ -13,54 +13,9 @@ class App
     @rentals = []
   end
 
-  def show_menu
-    puts 'Welcome to school library app!'
-    puts ''
-    puts 'Please choose an option by entering a number:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person ID'
-    puts '7 - Exit'
-    @number = gets
-    until @number.to_i.between?(1, 7)
-      puts 'Please write a number between 1 and 7'
-      @number = gets
-    end
-  end
-
-  def options
-    case @number.to_i
-    when 1
-      list_all_books
-      run
-    when 2
-      list_all_people
-      run
-    when 3
-      create_person
-      run
-    when 4
-      create_book
-      run
-    when 5
-      create_rental
-      run
-    when 6
-      list_all_rentals_for_id
-      run
-    end
-  end
-
   def run
     show_menu
-    if @number.to_i == 7
-      exit
-    else
-      options
-    end
+    puts 'Thank you for using this app!' if @number.to_i == 7
   end
 
   def create_person
--- main.rb
+++ main.rb
@@ -1,8 +1,50 @@
 require './app'
 
 def main
+  puts 'Welcome to school library app!'
   app = App.new
   app.run
+end
+
+def options
+  case @number.to_i
+  when 1
+    list_all_books
+    show_menu
+  when 2
+    list_all_people
+    show_menu
+  when 3
+    create_person
+    show_menu
+  when 4
+    create_book
+    show_menu
+  when 5
+    create_rental
+    show_menu
+  when 6
+    list_all_rentals_for_id
+    show_menu
+  end
+end
+
+def show_menu
+  puts ''
+  puts 'Please choose an option by entering a number:'
+  puts '1 - List all books'
+  puts '2 - List all people'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create a rental'
+  puts '6 - List all rentals for a given person ID'
+  puts '7 - Exit'
+  @number = gets
+  until @number.to_i.between?(1, 7)
+    puts 'Please write a number between 1 and 7'
+    @number = gets
+  end
+  options
 end
 
 main
