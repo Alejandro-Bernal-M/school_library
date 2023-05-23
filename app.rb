6fb731c626f9d3d5c588396cfc9da33520d0ba49
sanieni6 <luissanchezzamora23@gmail.com>
2023-05-23 11:23:37 -0500

    Create store books

--- app.rb
+++ app.rb
@@ -24,6 +24,7 @@ class App
   def run
     show_menu
     save_persons
+    save_books
     puts 'Thank you for using this app!' if @number.to_i == 7
   end
 end
--- modules/store.rb
+++ modules/store.rb
@@ -6,4 +6,10 @@ module Store
       f.write @people.map { |person| person_to_hash person }.to_json
     end
   end
+
+  def save_books
+    File.open('data/books.json', 'w')  do |f|
+      f.write @books.map { |book| book_to_hash book }.to_json
+    end
+  end
 end
