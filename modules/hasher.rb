2dbb04b88c02b5520282396460c42508131f1737
alejandro <alejandrober96@gmail.com>
2023-05-23 10:34:42 -0500

    Add rentals_array to person and books

--- modules/hasher.rb
+++ modules/hasher.rb
@@ -1,12 +1,15 @@
 module Hasher
 def person_to_hash person
+  array_of_rentals = []
+  person.rentals.each { |rental| array_of_rentals << rental_to_hash rental}
   if person.class == Student
   {
     id: person.id,
     age: person.age,
     name: person.name,
     parent_permission: person.parent_permission,
-    classroom: person.classroom	
+    classroom: person.classroom
+    rentals:  array_of_rentals
   }
   else
   {
@@ -14,14 +17,18 @@ def person_to_hash person
     age: person.age,
     name: person.name,
     specialization: person.specialization
+    rentals:  array_of_rentals
   }
   end
 end
 
 def book_to_hash book
+  array_of_rentals = []
+  book.rentals.each { |rental| array_of_rentals << rental_to_hash rental}
   {
     title: book.title,
-    author: book.author
+    author: book.author,
+    rentals:  array_of_rentals
   }
 end
 
